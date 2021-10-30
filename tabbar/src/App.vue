deabb95eea4ca054be2a24dad682c889c54a264c
fenda <fenda@gmail.com>
2021-10-30 14:07:20 +0800

    备份1

---
+++ tabbar/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <tab-bar>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/home.svg">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/all-fill.svg">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/cart-full.svg">
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+      <tab-bar-item>
+        <img slot="item-icon" src="./assets/img/tabbar/account.svg">
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+  import TabBar from "./components/tabbar/TabBar";
+  import TabBarItem from "./components/tabbar/TabBarItem";
+  export default {
+    name: 'App',
+    components: {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style>
+  @import "./assets/css/base.css";
+
+</style>
